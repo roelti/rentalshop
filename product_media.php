869987e5b03a88974dfa50d803eb6b5d45697ab3
Olaf551 <poma.oma@gmail.com>
2017-02-22 20:23:32 +0100

    Media opgelost

--- product_media.php
+++ product_media.php
@@ -12,48 +12,48 @@
             mkdir(ABSPATH.$artDir);
         }
         $ext = array_pop(explode(".", $fileUrl));
+        if ($ext == '')
+            return;
         $new_file_name = 'media-'.$post_id.'.'.$ext;
+        $targetUrl = ABSPATH.$artDir.$new_file_name;
+		copy($fileUrl, $targetUrl);
+		$siteurl = get_option('siteurl');
+		$file_info = getimagesize($targetUrl);
 
-        if(@fclose(@fopen($fileUrl, "r"))){
-            copy($fileUrl, ABSPATH.$artDir.$new_file_name);
-            $siteurl = get_option('siteurl');
-            $file_info = getimagesize(ABSPATH.$artDir.$new_file_name);
+		# Create an array of attachment data to insert into wp_posts table
+		$artdata = array(
+			'post_author' => 1,
+			'post_date' => current_time('mysql'),
+			'post_date_gmt' => current_time('mysql'),
+			'post_title' => $new_file_name,
+			'post_status' => 'inherit',
+			'comment_status' => 'closed',
+			'ping_status' => 'closed',
+			'post_name' => sanitize_title_with_dashes(str_replace("_", "-", $new_file_name)),
+			'post_modified_gmt' => current_time('mysql'),
+			'post_parent' => $post_id,
+			'post_type' => 'attachment',
+			'guid' => $siteurl.'/'.$artDir.$new_file_name,
+			'post_mime_type' => $file_info['mime'],
+			'post_excerpt' => '',
+			'post_content' => ''
+		);
 
-            # Create an array of attachment data to insert into wp_posts table
-            $artdata = array(
-                'post_author' => 1,
-                'post_date' => current_time('mysql'),
-                'post_date_gmt' => current_time('mysql'),
-                'post_title' => $new_file_name,
-                'post_status' => 'inherit',
-                'comment_status' => 'closed',
-                'ping_status' => 'closed',
-                'post_name' => sanitize_title_with_dashes(str_replace("_", "-", $new_file_name)),
-                'post_modified_gmt' => current_time('mysql'),
-                'post_parent' => $post_id,
-                'post_type' => 'attachment',
-                'guid' => $siteurl.'/'.$artDir.$new_file_name,
-                'post_mime_type' => $file_info['mime'],
-                'post_excerpt' => '',
-                'post_content' => ''
-            );
+		$uploads = wp_upload_dir();
+		$save_path = $uploads['basedir'].'/rentman/'.$new_file_name;
 
-            $uploads = wp_upload_dir();
-            $save_path = $uploads['basedir'].'/rentman/'.$new_file_name;
+		# Insert database record
+		$attach_id = wp_insert_attachment($artdata, $save_path, $post_id);
 
-            # Insert database record
-            $attach_id = wp_insert_attachment($artdata, $save_path, $post_id);
+		if ($attach_data = wp_generate_attachment_metadata($attach_id, $save_path)){
+			wp_update_attachment_metadata($attach_id, $attach_data);
+		}
 
-            if ($attach_data = wp_generate_attachment_metadata($attach_id, $save_path)){
-                wp_update_attachment_metadata($attach_id, $attach_data);
-            }
-
-            # Make it the featured image of the post it is attached to
-            $wpdb->insert($wpdb->prefix.'postmeta', array(
-                'post_id' => $post_id,
-                'meta_key' => '_thumbnail_id',
-                'meta_value' => $attach_id));
-        }
+		# Make it the featured image of the post it is attached to
+		$wpdb->insert($wpdb->prefix.'postmeta', array(
+			'post_id' => $post_id,
+			'meta_key' => '_thumbnail_id',
+			'meta_value' => $attach_id));
     }
 
     # Returns list of image file url's for every product
