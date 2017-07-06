9332a71cc6d24ce52f51ca18030ad7b70536594e
Roel  Tinkhof <SeptemberRoel@MacBook-Pro-van-Roel-5.local>
2017-07-06 15:57:28 +0200

    Verhuurdatum aanpasbaar maken en rentmanvelden gewrapped in div

--- product_availability.php
+++ product_availability.php
@@ -16,6 +16,8 @@
                     break;
                 }
             }
+
+            echo '<div class="rentman-fields">';
             # If there isn't, display the date input fields
             if ($rentableProduct == false){
                 $dates = get_dates();
@@ -48,6 +50,7 @@
             } else{
                 echo '<p class="availLog" hidden></p>';
             }
+            echo '</div>';
         }
     }
 
@@ -65,27 +68,29 @@
         }
         # If it does, add the date fields
         if ($rentableProduct){
-            ?><p>
-            <?php _e('<h2>VERHUURPERIODE</h2>','rentalshop');
-            $dates = get_dates();
-            $startdate = $dates['from_date'];
-            $enddate = $dates['to_date'];
-            $sdate =& $startdate;
-            $edate =& $enddate;
-            ?>
-            <form method="post">
-            <?php _e('Van:', 'rentalshop');?>
-            <input type="date" name="start-date" value="<?php echo $startdate;?>" min="<?php echo $today;?>">
-            <?php _e('Tot:', 'rentalshop');?>
-            <input type="date" name="end-date" value="<?php echo $enddate;?>" min="<?php echo $today;?>"><br>
+            if (apply_filters('rentman/show_cart_dates', true)) {
+                ?><p>
+                <?php _e('<h2>VERHUURPERIODE</h2>','rentalshop');
+                $dates = get_dates();
+                $startdate = $dates['from_date'];
+                $enddate = $dates['to_date'];
+                $sdate =& $startdate;
+                $edate =& $enddate;
+                ?>
+                <form method="post">
+                <?php _e('Van:', 'rentalshop');?>
+                <input type="date" name="start-date" value="<?php echo $startdate;?>" min="<?php echo $today;?>">
+                <?php _e('Tot:', 'rentalshop');?>
+                <input type="date" name="end-date" value="<?php echo $enddate;?>" min="<?php echo $today;?>"><br>
 
-            <!-- Update Button --></p>
-            <input type="hidden" name="rm-update-dates">
-            <input type="submit" class="button button-primary" value="<?php _e('Update Huurperiode', 'rentalshop');?>">
-            <input type="hidden" name="backup-start" value="<?php echo $sdate;?>">
-            <input type="hidden" name="backup-end" value="<?php echo $edate;?>">
-            </form>
-            <?php
+                <!-- Update Button --></p>
+                <input type="hidden" name="rm-update-dates">
+                <input type="submit" class="button button-primary" value="<?php _e('Update Huurperiode', 'rentalshop');?>">
+                <input type="hidden" name="backup-start" value="<?php echo $sdate;?>">
+                <input type="hidden" name="backup-end" value="<?php echo $edate;?>">
+                </form>
+                <?php
+            }
 
             # If 'Update Dates' button has been pressed, call update_dates function
             if (isset($_POST['rm-update-dates'])){
